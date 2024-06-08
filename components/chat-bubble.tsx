c793c1af0784045c67b0a33d4723d2e5ef39091b
BhanukaAGH <abewickramah1@gmail.com>
2024-06-08 23:43:53 +0530

    chore: update chat bubble design

--- components/chat-bubble.tsx
+++ components/chat-bubble.tsx
@@ -1,6 +1,10 @@
 import { Message } from 'ai/react'
+import Markdown from 'react-markdown'
+
+import { cn } from '@/lib/utils'
 
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import { Avatar, AvatarImage } from './ui/avatar'
 
 interface ChatBubbleProps extends Partial<Message> {}
 
@@ -10,19 +14,34 @@ const ChatBubble = ({ role = 'assistant', content }: ChatBubbleProps) => {
   return (
     <div>
       <Card>
-        <CardHeader>
+        <CardHeader className='pb-3'>
           <CardTitle
-            className={
+            className={cn(
+              'flex items-center space-x-3 text-lg',
               role != 'assistant'
                 ? 'text-amber-500 dark:text-amber-200'
                 : 'text-blue-500 dark:text-blue-200'
-            }
+            )}
           >
-            {role === 'assistant' ? 'AI' : 'You'}
+            {role === 'assistant' ? (
+              <>
+                <Avatar className='w-8 h-8'>
+                  <AvatarImage src='/chatbot.png' alt='chatbot' />
+                </Avatar>
+                <span>Assistant</span>
+              </>
+            ) : (
+              <>
+                <Avatar className='w-8 h-8'>
+                  <AvatarImage src='/user.png' alt='user' />
+                </Avatar>
+                <span>You</span>
+              </>
+            )}
           </CardTitle>
         </CardHeader>
         <CardContent className='text-sm'>
-          <span>{content}</span>
+          <Markdown className='prose prose-invert'>{content}</Markdown>
         </CardContent>
       </Card>
     </div>
